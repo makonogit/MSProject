8369af1e134348dcf32a30008c170e20941217fd
Nakagawa <utt014257u6s02@outlook.jp>
2024-09-17 18:00:57 +0900

    Update：タグマネージャーの変更（Burstタグの追加） Add：フォルダーの追加 Add：はじけるオブジェクトプレハブ、スクリプトの追加 Add：破片のプレハブ、スクリプトの追加 Add：衝撃波のプレハブ、スクリプト、シェーダーグラフ、マテリアルの追加

---
+++ Assets/C#Script/Burst/CS_ShockWave.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class CS_ShockWave : MonoBehaviour 
+{
+    [SerializeField, Tooltip("消滅時間:\n消滅するまでの時間")]
+    private float DestroyTime;
+
+    [SerializeField, Tooltip("速度:\n")]
+    private float Speed;
+
+    private void Update()
+    {
+        Vector3 size = Vector3.one;
+
+        this.transform.localScale += size * (Time.deltaTime * Speed);
+
+        UntilDestroyMyself();
+    }
+
+    /// <summary>
+    /// 消滅するまでの処理
+    /// </summary>
+    private void UntilDestroyMyself()
+    {
+        DestroyTime -= Time.deltaTime;
+        // 破棄する
+        bool shouldDestroy = DestroyTime <= 0;
+        if (shouldDestroy) Destroy(this.gameObject);
+    }
+}
