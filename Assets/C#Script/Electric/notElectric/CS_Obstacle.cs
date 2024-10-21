094cb39083d438c46746ceb3cd37721b3c5cfafe
Nakagawa <utt014257u6s02@outlook.jp>
2024-10-21 22:16:10 +0900

    Add：センサーと障害物(未完成)を追加

---
+++ Assets/C#Script/Electric/notElectric/CS_Obstacle.cs
@@ -0,0 +1,56 @@
+﻿//-------------------------------
+// クラス名 :CS_Obstacle
+// 内容     :体力のある障害物
+// 担当者   :中川 直登
+//-------------------------------
+using UnityEngine;
+
+namespace Assets.C_Script.Electric.notElectric
+{
+    public class CS_Obstacle :MonoBehaviour
+    {
+        [SerializeField]
+        private int hp = 5;
+        // 読み取り用
+        public int HP { get { return hp; } }
+        [SerializeField]
+        private ParticleSystem DebrisParticle;
+
+        // スタート
+        private void Start(){}
+        // フィクスドアップデート
+        private void FixedUpdate()
+        {
+            if (ShouldExplosion) Explosion();
+        }
+        // アップデート
+        private void Update() {}
+
+       
+        /// <summary>
+        /// 爆発するべきか
+        /// </summary>
+        private bool ShouldExplosion{ get { return hp <= 0; }}
+
+        /// <summary>
+        /// 爆破-消滅するまで
+        /// </summary>
+        private void Explosion()
+        { 
+            Destroy(this.gameObject);
+        }
+
+        /// <summary>
+        /// ダメージを与える関数
+        /// </summary>
+        /// <param name="damage"></param>
+        public void HitDamages(int  damage = 0) 
+        {
+            hp -= damage;
+        }
+    }
+}
+//===============================
+// date : 2024/10/20
+// programmed by Nakagawa Naoto
+//===============================
