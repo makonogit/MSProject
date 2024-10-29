54dd577fb66bed24df626b8ab243980aca556294
fujiwara1229 <fujiwara.kosuke.1229.git@gmail.com>
2024-10-29 14:50:49 +0900

    Add:落下デス時のカメラワークを追加

---
+++ Assets/C#Script/Player/CS_DropGameOver.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_DropGameOver : MonoBehaviour
+{
+    private CS_CameraManager cameraManager;     // カメラマネージャー
+    private GameObject tpsCamera;               // TPSカメラ
+    private GameObject fallingCamera;           // 落下時カメラ
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraManager = GameObject.Find("CameraManager")?.GetComponent<CS_CameraManager>();
+        tpsCamera = GameObject.Find("TpsCamera");
+        fallingCamera = GameObject.Find("FallingCamera");
+        if (fallingCamera == null)
+        {
+            UnityEngine.Debug.LogError("FallingCameraをHierarchyに追加してください。");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // メインカメラの位置を自身に設定
+            fallingCamera.transform.position = tpsCamera.transform.position;
+            fallingCamera.transform.rotation = tpsCamera.transform.rotation;
+
+            // カメラの切り替え
+            cameraManager.SwitchingCamera(1);
+        }
+    }
+}
