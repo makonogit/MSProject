0f6cf066cac8f07e231588bdf74e80fb2e35ae94
Nakagawa <utt014257u6s02@outlook.jp>
2024-11-14 12:58:31 +0900

    Update:ファイルの打ち消し Add：トランポリンのアニメーションコントローラーを追加し忘れ分を追加

--- Assets/C#Script/Player/CSP_UseEnergyCore.cs
+++ Assets/C#Script/Player/CSP_UseEnergyCore.cs
@@ -80,7 +80,6 @@ public class CSP_UseEnergyCore : ActionBase
             GetAnimator().SetBool("Mount", false);
             // コアをセットする
             coreUnit.SetCore(csp_throwing.GetEnergyCore());
-            csp_throwing.GetEnergyCore().transform.parent = null;
             //csp_throwing.GetEnergyCore().transform.position = targetObject.transform.position;
             targetObject = null;
             coreUnit = null;
