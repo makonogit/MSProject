ed3741b9ec0c75646c429f3b23fbc72e4bce5869
Nakagawa <utt014257u6s02@outlook.jp>
2024-12-02 08:01:46 +0900

    Add：ダイアログの追加 Remove：ファイルの位置を変更

---
+++ Assets/C#Script/GameEvent/CSGE_MultiDialog.cs
@@ -0,0 +1,83 @@
+﻿//-------------------------------
+// クラス名 :CS_GameEvent 
+// 内容     :ゲームイベントの基底クラス
+// 担当者   :中川 直登
+//-------------------------------
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.C_Script.GameEvent
+{
+    internal class CSGE_MultiDialog : CS_GameEvent
+    {
+        [SerializeField]
+        private Image image;
+        [SerializeField]
+        private List<Sprite> sprites = new List<Sprite>();
+        [SerializeField]
+        private bool display = false;
+        [SerializeField]
+        private bool isOnce = false;
+        private bool end = false;
+        [SerializeField]
+        private Animator animator = null;
+        [SerializeField]
+        private CS_InputSystem inputSystem = null;
+        private int index = 0;
+        private int Index 
+        {
+            set 
+            {
+                index = Mathf.Max(Mathf.Min(value, sprites.Count - 1),0);
+            }
+            get { return index; }
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            if (sprites.Count > 0) image.sprite = sprites[0];
+        }
+
+        protected override void EventUpdate()
+        {
+            base.EventUpdate();
+            animator.SetBool("Display", true);
+            if (display) ChangeSprite();
+        }
+
+        protected override void Init()
+        {
+            base.Init();
+            if (end && isOnce) isFinish = true;
+            animator.SetBool("Close", false);
+        }
+
+        protected override void Uninit()
+        {
+            base.Uninit();
+            animator.SetBool("Close", true);
+            animator.SetBool("Display", false);
+            end = true;
+        }
+        private void ChangeSprite()
+        {
+            if (inputSystem.GetDpadLeftTriggered()) 
+            { 
+                Index--; 
+                image.sprite = sprites[index];
+            }
+            if (inputSystem.GetDpadRightTriggered()) 
+            {
+                Index++;
+                image.sprite = sprites[index];
+            }
+            if (index>= sprites.Count-1 && inputSystem.GetButtonBTriggered()) isFinish = true;
+        }
+    }
+}
+//===============================
+// date : 2024/12/01
+// programmed by Nakagawa Naoto
+//===============================
---
+++ Assets/C#Script/Player/CS_LockControl.cs
@@ -0,0 +1,24 @@
+﻿//-------------------------------
+// クラス名 :CS_LockControl
+// 内容     :プレイヤーの行動制限
+// 担当者   :中川 直登
+//-------------------------------
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.C_Script.Player
+{
+    internal class CS_LockControl : MonoBehaviour
+    {
+        [SerializeField]
+        private CSP_Jump jumpControl;
+        [SerializeField]
+        private CSP_Shot ShotControl;
+
+        private void Start() 
+        {
+            ShotControl.enabled = false;
+            jumpControl.enabled = false;
+        }
+    }
+}
