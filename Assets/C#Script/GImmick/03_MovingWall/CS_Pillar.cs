69e86e45d5fbdec789e7799ae268615c240c9dc2
Nakagawa <utt014257u6s02@outlook.jp>
2024-12-02 12:07:21 +0900

    Update：プレス機と動く壁のアニメーション追加

---
+++ Assets/C#Script/GImmick/03_MovingWall/CS_Pillar.cs
@@ -0,0 +1,29 @@
+﻿//-------------------------------
+// クラス名 :CS_PillarPressMachine
+// 内容     :プレス機の柱の処理
+// 担当者   :中川 直登
+//-------------------------------
+using UnityEngine;
+
+namespace Assets.C_Script.Gimmick
+{
+    internal class CS_Pillar : MonoBehaviour
+    {
+        private Vector3 firstPosition;
+        private Transform Wall;
+
+        private void Start()
+        {
+            Wall = transform.parent;
+            firstPosition = Wall.localPosition;
+        }
+
+        private void FixedUpdate()
+        {
+            Vector3 scale = this.transform.localScale;
+            scale.y = Wall.localPosition.y - firstPosition.y;
+            scale.y *= -0.005f;
+            this.transform.localScale = scale;
+        }
+    }
+}
